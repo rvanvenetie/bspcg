fc84ea08984fdef09aeb110cc9ae68c3e70a91ac
Jan Westerdiep <janner@gmail.com>
2015-01-03 12:42:53 +0100

    seq CG werkt nu

--- Sequential/main.c
+++ Sequential/main.c
@@ -2,24 +2,43 @@
 #include <math.h>
 #include <Accelerate/Accelerate.h>
 
-float cg( const int kmax, const float eps, const int n, const float **A, const float *b, const float *x0, float *x) {
+float cg( const int kmax, const float eps, //settings
+          const int n, const float A[n][n], const float b[n], const float x0[n], //input data
+          float x[n]) { //output data
   printf("Solving system Ax=b, dim=%i, kmax=%i, eps=%f, with\n", n, kmax, eps);
+
   printf("A = \n");
   for( int i = 0; i < n; i++) {
-    for( int j = 0; i < n; i++) {
-      printf("%.4f", A[i][j]);
+    for( int j = 0; j < n; j++) {
+      printf("%.4f ", A[i][j]);
     }
+    printf("\n");
   }
+  printf("\n");
+
+  printf("b = \n");
+  for( int i = 0; i < n; i++) {
+    printf("%.4f\n", b[i]);
+  }
+  printf("\n");
+
+  printf("x0 = \n");
+  for( int i = 0; i < n; i++) {
+    printf("%.4f\n", x0[i]);
+  }
+  printf("\n");
   int k = 0;
   float r[n];
   float rho;
   float rho_old;
   float normbsq;
 
+  cblas_scopy( n, x0, 1, x, 1); // x \to x_0
   cblas_scopy( n, b, 1, r, 1); // r \to b
+  cblas_ssymv( CblasRowMajor, 122, n, -1.0, (float *)A, n, x, 1, 1.0, r, 1); // r \to r - Ax
   rho = cblas_sdot( n, r, 1, r, 1); //rho \to <r, r>
   normbsq = cblas_sdot( n, b, 1, b, 1); //find <b, b>
-  cblas_scopy( n, x0, 1, x, 1); // x \to x_0
+
 
 
   while( rho > eps*eps*normbsq && k < kmax) {
@@ -48,19 +67,30 @@ float cg( const int kmax, const float eps, const int n, const float **A, const f
     k = k+1;
   }
 
+  printf("Solution found! rho = %f\n", rho);
+  printf("x = \n");
+  for( int i = 0; i < n; i++) {
+    printf("%.4f\n", x[i]);
+  }
+  printf("\n");
+
+  float b2[n];
+  cblas_ssymv( CblasRowMajor, 122, n, 1.0, (float *)A, n, x, 1, 0.0, b2, 1); //b2 \to Ax
+
+  printf("Ax = \n");
+  for( int i = 0; i < n; i++) {
+    printf("%.4f\n", b2[i]);
+  }
+  printf("\n");
   return rho;
 }
 
 int main() {
-  float x0[2] = {1.0, 2.0};
-  float a[2][2] = {{4.0, 1.0}, {1.0, 3.0}};
+  float x0[2] = {2.0, 1.0};
+  float a[2][2] = {{2.0, 1.0}, {1.0, 3.0}};
   float b[2] = {1.0, 2.0};
   float x[2] = {0.0, 0.0};
 
   float end_roundoff = cg( 2, 0.001, 2, a, b, x0, x);
-
-  printf("%f\n", x[0]);
-  printf("%f\n", x[1]);
-  printf("%f\n", end_roundoff);
   return 0;
 }
