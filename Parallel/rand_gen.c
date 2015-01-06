9929f62b8c183dbf151c8ced0129c14571f25de2
Student v. R. Bisseling <bissstud@int1.cartesius.surfsara.nl>
2015-01-06 12:34:54 +0100

    Random generator

---
+++ Parallel/rand_gen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include "io.h"
+#include "vec.h"
+#include "bspmv.h"
+#include "bspip.h"
+#include "bspedupack.h"
+
+typedef struct mat{
+  double * val;
+  double * i, *j;
+} mat;
+
+int main(void) {
+
+
+
+
+}
