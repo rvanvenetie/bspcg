dd4f4518270a37f814ff45225acb713fbb218d63
Jan Westerdiep <janner@gmail.com>
2015-01-06 17:01:23 +0100

    ik push

--- Parallel/libs/vec.h
+++ Parallel/libs/vec.h
@@ -1,4 +1,5 @@
 int vec_print( const char *name, const int n, const double *x);
+void vec_random( const int n, const int seed, double *p);
 void vec_copy( const int n, const double *r, double *p);
 void vec_scale( const int n, const double beta, double *p);
 void vec_axpy( const int n, const double alpha, const double *w, double *r);
