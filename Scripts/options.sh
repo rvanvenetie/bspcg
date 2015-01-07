4c677c797fadb3f613ea9ce01c956088ea3966da
Student v. R. Bisseling <bissstud@int1.cartesius.surfsara.nl>
2015-01-07 11:18:48 +0100

    latest option

--- Scripts/options.sh
+++ Scripts/options.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+if [[ `whoami` == "bissstud" ]]; then
+	cd $HOME/Students14/Jan/bspcg/Scripts/
+	echo "Running on Cartesius $@"
+	runcmd="srun"
+else
+	echo "Running locally $@"
+	runcmd=""
+fi
+echo -e "\n\033[1;31mtime\t`date`\033[0;39m"
+
 narr=(16 32 64)
 sparr=(0.01)
 parr=(1 2 4)
-narr=(16 32 64 128 256 512 1024 2048 4096)
+narr=(64 128 256 512 1024 2048 4096)
 sparr=(0.01 0.02 0.03 0.04 0.05 0.1 0.2)
 parr=(1 2 4 8 16 32 64)
 kmax=100000
