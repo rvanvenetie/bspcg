a2985dd9455154396748952f2af283ec3101cb6c
Student v. R. Bisseling <bissstud@int1.cartesius.surfsara.nl>
2015-01-07 10:58:04 +0100

    Scripts for the cartesius

--- Scripts/bspcg_test.sh
+++ Scripts/bspcg_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cwd=$(pwd)
+#SBATCH -n 64
+#SBATCH -t 30:00
+runcmd="srun"
+cwd=/home/bissstud/Students14/Jan/bspcg/Scripts
 bspcg="$cwd/../Parallel/main"
 matdir="$cwd/../mtxMatrices"
 source options.sh
@@ -7,12 +10,11 @@ for n in "${narr[@]}"
 do
   for sp in "${sparr[@]}"
 	do
-	  ./genmat.sh $n $sp
 	  for p in "${parr[@]}"
 		do
 			matname="randmat_${n}_${sp/./_}"
 			matfile="$matdir/$matname/$matname.mtx"
-			$bspcg $matfile $loadb $p $kmax $eps $time $debug
+			$runcmd $bspcg $matfile $loadb $p $kmax $eps $time $debug 
 		done
 	done
 done
