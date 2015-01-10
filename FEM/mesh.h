ef2e97b107aaa7166aa3e3a2210a1e25b4619e99
Raymond van Venetie <rayvanve@gmail.com>
2015-01-10 16:09:51 +0100

    Header mesh

---
+++ FEM/mesh.h
@@ -0,0 +1,17 @@
+#ifndef MESH_H
+#define MESH_H
+typedef struct {
+  int n, m, nz, lennz;
+  int *I, *J;
+  double *val;
+} matrix_s;
+
+typedef struct {
+  int nverts, ntris, P;
+  double *x, *y; //x and y coords
+  int *b; //boundary
+  int *t; //tris coords, len = ntris*3
+  int *d; //distribution
+  matrix_s *hypergraph;
+} mesh_s;
+#endif
