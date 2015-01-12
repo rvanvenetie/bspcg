4cb6c37891e701097b2e01b7998364289f374d86
Jan Westerdiep <janner@gmail.com>
2015-01-12 13:37:46 +0100

    heir dan

--- FEM/m2dm.sh
+++ FEM/m2dm.sh
@@ -5,5 +5,6 @@ mtx2dm="$cwd/../FEM/mtx2dm"
 mesh=$1
 P=$2
 $m2mtx $mesh.m > $mesh.mtx
-$mond $mesh.mtx $2 0.1
+$mond $mesh.mtx $2 0.03
 $mtx2dm $mesh.m $2 > $mesh.m-P$2
+rm *.mtx*
---
+++ FEM/plottri.py
@@ -0,0 +1,55 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+fn = sys.argv[1] if len( sys.argv) > 1 else "lshaped.m"
+
+def readmesh( fn):
+    fp = open( fn)
+
+    distributed = 0
+
+    if fp.readline().find("distributed") > 0:
+        distributed = 1
+
+    if distributed:
+        P = 0
+
+    line = fp.readline().split(" ")
+    nverts = int(line[0])
+    ntris = int(line[1])
+    if distributed:
+        P = int(line[2])
+
+    x = np.zeros(nverts)
+    y = np.zeros(nverts)
+    b = np.zeros(nverts)
+
+    for i in range( nverts):
+        [x[i], y[i], b[i]] = fp.readline().split(" ")
+
+    t = np.zeros([ntris, 3])
+    p = np.zeros(ntris)
+
+    for i in range( ntris):
+        if distributed:
+            [t[i][0], t[i][1], t[i][2], p[i]] = fp.readline().split(" ")
+        else:
+            [t[i][0], t[i][1], t[i][2]] = fp.readline().split(" ")
+            print t[i]
+
+    if distributed:
+        return (1, nverts, ntris, x, y, b, t, P, p)
+    else:
+        return (0, nverts, ntris, x, y, b, t, 1, p)
+
+mesh = readmesh( fn)
+print mesh
+distributed = mesh[0]
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_aspect( 'equal')
+plt.tripcolor( mesh[3], mesh[4], mesh[6], 'go-', facecolors=mesh[8])
+plt.show()
