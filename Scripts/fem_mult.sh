59635678d9f99bd0dd837c9a2df504bf715597c8
Raymond van Venetie <rayvanve@gmail.com>
2015-01-13 13:58:01 +0100

    Fem_mult works

--- Scripts/fem_mult.sh
+++ Scripts/fem_mult.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source options.sh
-for proc in "${parr[@]}$"
+./m2fem.sh $1 
+for proc in "${parr[@]}"
 do
 	./m2dm.sh $1 $proc
-	./m2fem.sh $1 $proc
-	./mondriaan_mult.sh $1 $proc
+	./mondriaan.sh $1 $proc
 done
